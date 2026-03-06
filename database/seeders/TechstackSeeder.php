db74eaea014bc8ec3d91beac26af626712ce728d
KuroiiDev <maitsamkadzim26@gmail.com>
2026-03-06 22:10:58 +0700

    Init

---
+++ database/seeders/TechstackSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TechstackSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
